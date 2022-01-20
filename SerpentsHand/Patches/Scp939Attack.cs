22fe2f2e4d02ae5f21c52ce02133305629b9fb1b
Alexander Young <alex1001@live.ca>
2022-01-20 10:33:13 -0700

    this

--- SerpentsHand/Patches/Scp939Attack.cs
+++ SerpentsHand/Patches/Scp939Attack.cs
@@ -20,18 +20,6 @@ namespace SerpentsHand.Patches
 		public static bool Prefix(ReferenceHub target) => !EventHandlers.shPlayers.Contains(Player.Get(target)) || (EventHandlers.shPlayers.Contains(Player.Get(target)) && SerpentsHand.instance.Config.FriendlyFire) || (EventHandlers.RoundEnded && SerpentsHand.instance.Config.EndRoundFriendlyFire);
 	}
 
-	/*
-	[HarmonyPatch(typeof(PlayableScps.Scp173), nameof(PlayableScps.Scp173.UpdateObservers))]
-	class Scp173LookingPatch
-	{
-		public static void Postfix(PlayableScps.Scp173 __instance)
-		{
-			if (__instance._isObserved) __instance._observingPlayers.RemoveWhere(target => EventHandlers.shPlayers.Contains(Player.Get(target)) && SerpentsHand.instance.Config.FriendlyFire);
-			__instance._isObserved = (__instance._observingPlayers.Count > 0 || __instance.StareAtDuration > 0f);
-		}
-	}
-	*/
-
 	[HarmonyPatch(typeof(PlayerStatsSystem.AttackerDamageHandler), nameof(PlayerStatsSystem.AttackerDamageHandler.ProcessDamage))]
 	class FriendlyFirePatch
 	{
